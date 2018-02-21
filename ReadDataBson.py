c55a9c5d5d3ac8b9968b0c4e5b8af1aa3769905b
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2018-02-20 21:18:32 -0500

    clean up

--- ReadDataBson.py
+++ ReadDataBson.py
@@ -15,42 +15,43 @@ import os
 client = MongoClient()
 database = client.github_16
 events = database.events
-users = database.users
-repo = database.repos
 
 # client id and client secret are used in calling the github API
 # they will help to raise the maximum limit of calls per hour
+# note: you will need your private txt file that includes the private keys
 file=open("privateVar.txt",'r').read()
 client_id = file.split('\n', 1)[0]
 client_secret = file.split('\n', 1)[1]
 
 
-def print_first_elem(collection):
-    for elem in collection.find():
+def print_first_elem():
+    for elem in events.find():
           pprint.pprint(elem)
           break
-    print collection.count()
+    print events.count()
 
-def print_in_range(collection, offset, position):
-    for elem in collection.find()[offset:position]:
-          # pprint.pprint(elem)
-          print elem['watchers_count']
+def print_in_range(offset, position):
+    for elem in events.find()[offset:position]:
+          pprint.pprint(elem)
+          # print elem['watchers_count']
           print_separator()
 
 
-def print_with_condition(collection, field, value):
-    for elem in collection.find():
+def print_with_condition(field, value):
+    for elem in events.find():
       if elem[field] == value:
         pprint.pprint(elem)
 
+# prints dashes as separator, only to beautify the prints
 def print_separator():
-    print ('---------------------------------------------------------------------------')
+    print ('-'*100)
 
 # fetches the followers of a user from a url and create a list
 def actor_followers_list(url):
     followers_list = []
+    new_url = add_client_id_client_secret_to_url(url)
     try:
-        response = urllib2.urlopen(url)
+        response = urllib2.urlopen(new_url)
         data = simplejson.load(response)
         for actor in data:
             followers_list.append(actor['login'])
@@ -60,22 +61,21 @@ def actor_followers_list(url):
 
 # fetches the user object from a url and calls the followers list creator
 def read_actor_login(url):
+    new_url = add_client_id_client_secret_to_url(url)
     try:
-        response = urllib2.urlopen(url)
+        response = urllib2.urlopen(new_url)
         data = simplejson.load(response)
         return actor_followers_list(data['followers_url'])
     except urllib2.URLError, e:
         return 'null'
 
+# appends the client id and the client secret to urls
+def add_client_id_client_secret_to_url(url):
+    return url+'?client_id='+client_id+'&client_secret='+client_secret
 
+
+# to create a sparate database out of the downloaded github bson file only for our work
 def create_database():
-
-    actor = ''
-    repo = ''
-    followers = []
-    event_type = ''
-    followers_count = 0
-
     for event in events.find():
         repo = event['repo']['name'].split('/')[1]
         event_type = event['type']
@@ -84,5 +84,8 @@ def create_database():
         print repo,' - ', event_type, ' - ', actor, ' - ' , followers
 
 
-
-print client_secret
+index = 1
+for elem in events.find():
+  if elem['type'] == 'ForkEvent':
+    index += 1
+    print index
