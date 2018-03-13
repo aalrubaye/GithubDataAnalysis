bb56863d02afb3e48e9a59a2e9b4ff043734b5c1
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2018-03-12 20:34:52 -0400

    Code update

--- CreateModel.py
+++ CreateModel.py
@@ -15,15 +15,19 @@ class ModelGenerator:
     graph = nx.Graph()
     # number of the times we aim to generate extra models
     models_number = 2
+    limit = 100
+    is_random = True
+    time = 0
+    forks = []
+    repos = []
 
     def __init__(self):
         # connects to the db
         self.final_collection = MongoConnection.connect()
         # True = random
         # False = Prob_random
-        self.generate_graph(0, False)
-        # for i in range(10):
-        #     print self.pick([0.1,0.3,0.1,0.1,0.3],[0,1,2,3])
+        self.generate_graph(False)
+
 
     # Prepares and adds a node + attributes to the graph
     def add_node(self, entry, label, node_type):
@@ -48,19 +52,16 @@ class ModelGenerator:
                             node_type=node_type)
 
     # generates the model
-    def generate_graph(self, time, random):
+    def generate_graph(self, rnd):
+        self.is_random = rnd
 
         index = 0
-
-        forks = []
-        followers = []
-        repos = []
 
         for entry in self.final_collection.find():
             # add the repo node + adding some attributes to the node
             self.add_node(entry, entry['repo_name'], self.node_type('Repo'))
-            forks.append(entry['repo_forks_count'])
-            repos.append(entry['repo_name'])
+            self.forks.append(entry['repo_forks_count'])
+            self.repos.append(entry['repo_name'])
 
             # add the actor node
             self.add_node(entry, entry['actor'], self.node_type('Actor'))
@@ -68,6 +69,34 @@ class ModelGenerator:
             # connect actor node -> repo node
             self.graph.add_edge(entry['repo_name'], entry['actor'])
 
+            index += 1
+            print index
+            if index == self.limit:
+                break
+
+        self.repo_repo()
+
+    # If a user forked more than a repo, this def will connects them
+    def repo_repo(self):
+        for node in self.graph.degree:
+            if node[1] > 1:
+                rp_to_be_connected = []
+                for rp in self.graph.edges(node[0]):
+                    rp_to_be_connected.append(rp[1])
+                rp_len = len(rp_to_be_connected)
+                for k in range(rp_len):
+                    for j in range(k+1,rp_len):
+                        rp1 = rp_to_be_connected[k]
+                        rp2 = rp_to_be_connected[j]
+                        self.graph.add_edge(rp1, rp2)
+        self.follower_actor()
+
+    # Connects the followers to users
+    def follower_actor(self):
+        print ('Followers')
+        followers = []
+        index = 0
+        for entry in self.final_collection.find():
             flwrs = []
             # add the followers node
             # connect followers node -> actor node
@@ -78,17 +107,17 @@ class ModelGenerator:
             followers.append(flwrs)
             index += 1
             print index
-            if index == 10:
+            if index == self.limit:
                 break
 
         # exports graph as a graphml file
-        nx.write_graphml(self.graph, "/Users/Abduljaleel/Desktop/"+self.model_name(random, time)+".graphml")
-        time += 1
+        nx.write_graphml(self.graph, "/Users/Abduljaleel/Desktop/"+self.model_name(self.is_random, self.time)+".graphml")
+        self.time += 1
         # generates a second model with more connection but same nodes
-        self.add_edges_to_graph(forks, followers, repos, random, time)
+        self.add_edges_to_graph(self.forks, followers, self.repos, self.time)
 
     # Adds edges to the graph based on different probabilities
-    def add_edges_to_graph(self, forks_list, followers, repos_list, random, time):
+    def add_edges_to_graph(self, forks_list, followers, repos_list, time):
         if self.models_number > 0:
             print('-'*100)
             print('Generating the model at time t'+str(time))
@@ -105,11 +134,14 @@ class ModelGenerator:
                     index_list.append(i)
 
             for i in range(len(forks)):
-                if random is False:
+                if self.is_random is False:
                     prob = self.compute_probabilities(forks, index_list)
                     index_of_rand, rand = self.weight_random_pick(prob, index_list)
                 else:
                     index_of_rand, rand = self.random_pick(index_list)
+                # print prob
+                # print index_list
+                # print rand
 
                 follower_to_connect = followers[rand].pop()
                 self.graph.add_edge(repos[rand], follower_to_connect)
@@ -117,14 +149,15 @@ class ModelGenerator:
                 self.graph.node[follower_to_connect]['node_type'] = self.node_type('Actor')
                 forks[rand] += 1
                 if len(followers[rand]) == 0:
+                    # print ('done')
                     index_list.pop(index_of_rand)
-
+                # print ('-'*100)
                 print i
 
             # export graph as a graphml file
-            nx.write_graphml(self.graph, "/Users/Abduljaleel/Desktop/"+self.model_name(random, time)+".graphml")
+            nx.write_graphml(self.graph, "/Users/Abduljaleel/Desktop/"+self.model_name(self.is_random, time)+".graphml")
             self.models_number -= 1
-            self.add_edges_to_graph(forks_list, followers, repos_list, random, time+1)
+            self.add_edges_to_graph(forks_list, followers, repos_list, time+1)
 
     # Computes the probabilities of a set of numbers
     def compute_probabilities(self, forks, index):
@@ -141,20 +174,9 @@ class ModelGenerator:
     # Picks a random number
     def weight_random_pick(self, prob_list, index_list):
         rand = np.random.choice(len(prob_list), 1, p=prob_list)[0]
-        while rand not in index_list:
-            rand = np.random.choice(len(prob_list), 1, p=prob_list)[0]
-            print ('WWWWWWWWWWW')
+        # while rand not in index_list:
+        #     rand = np.random.choice(len(prob_list), 1, p=prob_list)[0]
         return rand, index_list[rand]
-
-    def pick(self, val_list, index_list):
-        x = random.uniform(0, 1)
-        cumulative_probability = 0.0
-        for item, item_probability in zip(index_list, val_list):
-            cumulative_probability += item_probability
-            if x < cumulative_probability:
-                break
-        return item, index_list[item]
-
 
 
     def random_pick(self, index_list):
@@ -203,7 +225,6 @@ class ModelGenerator:
             return "random_model_t"+str(time)
         else:
             return "prob_model_t"+str(time)
-
 
 if __name__ == "__main__":
     ModelGenerator()
